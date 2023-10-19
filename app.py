473649674d6a5263913c1319512f275fc6209fc3
honlnm <nicole.m.honl@gmail.com>
2023-10-19 12:11:23 -0500

    score, timer, styling

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+from flask import Flask, render_template, redirect, request, session, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
-boggle_game = Boggle()
+app = Flask(__name__)
+app.config['SECRET_KEY']="safd1561Awa515a"
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def home_page():
+    boggle_game = Boggle()
+    new_board = boggle_game.make_board()
+    session['board'] = new_board
+    return render_template('index.html', board = new_board)
+
+@app.route("/check-word", methods=["GET"])
+def check_guess():
+    guess = request.args['currentGuess']
+    board = session['board']
+    words = Boggle()
+    result = words.check_valid_word(board, guess)
+    return jsonify({'result': result})
+
+@app.route("/score", methods=["GET"])
+def store_score():
+    score = request.args['score']
+    
+
---
+++ static/boggle.js
@@ -0,0 +1,88 @@
+$('input').attr('disabled', 'disabled');
+
+//starting values:
+$('#timer').text(60)
+$('#score').text(0)
+
+//msg------------------
+$('#guess_form').submit(function (evt) {
+    const $guess = $('#guess_input_box').val();
+    $.ajax({
+        type: "GET",
+        url: '/check-word',
+        data: { currentGuess: $guess },
+        success: getResponse()
+    });
+    $('#guess_input_box').val("")
+    evt.preventDefault();
+});
+
+function addMsg() {
+    const msg = $("<p id='temp_msg'></p>")
+    $('#msg').append(msg)
+}
+
+function rmvMsg() {
+    setTimeout(function () {
+        $('#temp_msg').remove()
+    },
+        3000
+    )
+}
+
+//show message based on server response
+async function getResponse() {
+    const $guess = $('#guess_input_box').val()
+    const resp = await axios.get('/check-word', { params: { currentGuess: $guess } });
+    if (resp.data.result === "not-on-board") {
+        addMsg()
+        $('#temp_msg').text(`The word '${$guess}' is not on the board`)
+        rmvMsg()
+    } else if (resp.data.result === "not-word") {
+        addMsg()
+        $('#temp_msg').text(`The word '${$guess}' does not exist`)
+        rmvMsg()
+    } else {
+        addMsg()
+        $('#temp_msg').text(`The word '${$guess}' has been found`)
+        rmvMsg()
+        //Score----------------
+        let newScore = parseInt($("#score").text()) + $guess.length;
+        $('#score').text(newScore)
+    }
+}
+
+//Timer----------------
+
+function timer() {
+    --totalSeconds
+    if (totalSeconds < 10) totalSeconds = "0" + totalSeconds;
+    $('#timer').text(totalSeconds)
+}
+
+function timerRundown() {
+    timesRun = 0;
+    let timerStart = setInterval(function () {
+        timesRun += 1;
+        if (timesRun === 60) {
+            clearInterval(timerStart);
+            $('#start').removeAttr('disabled');
+            $('input').attr('disabled', 'disabled');
+            $.ajax({
+                type: "GET",
+                url: '/score',
+                data: { score: $('#score').text() },
+            });
+        }
+        timer()
+    }, 1000);
+    timerStart
+}
+
+$('#start').on("click", function () {
+    $('input').removeAttr('disabled');
+    $('#start').attr('disabled', 'disabled');
+    $('#score').text(0)
+    totalSeconds = 60;
+    timerRundown()
+})
