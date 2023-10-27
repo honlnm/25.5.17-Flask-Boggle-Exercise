6ea6ef2d07626154935ec103be90aef81b45bb4d
honlnm <nicole.m.honl@gmail.com>
2023-10-27 13:20:38 -0500

    finished test files

--- tests/test.py
+++ tests/test.py
@@ -8,5 +8,33 @@ app.config['DEBUG_TB_HOSTS']=['dont-show-debug-toolbar']
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+        
+    def test_home_page(self):
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIn('high-score', session)
 
+    def test_valid_word(self):
+        with self.client as client:
+            with client.session_transaction() as session:
+                session['board'] = [["C", "A", "T", "T", "T"],
+                                    ["C", "A", "T", "T", "T"],
+                                    ["C", "A", "T", "T", "T"],
+                                    ["C", "A", "T", "T", "T"],
+                                    ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?currentGuess=cat')
+        self.assertEqual(response.json['result'], 'ok')
+
+    def test_invalid_word(self):
+        self.client.get('/')
+        response = self.client.get('/check-word?currentGuess=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')    
+
+    def test_nonsense_word(self):
+        self.client.get('/')
+        response = self.client.get('/check-word?currentGuess=skalgheaagofdg')
+        self.assertEqual(response.json['result'], 'not-word')    
